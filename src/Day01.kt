a9673bfccddc163ea3414260a1c2ea985b368406
jarmstrong <itsjarm@gmail.com>
2022-12-02 00:44:39 +1100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,29 @@
 fun main() {
+    fun calorieCountsByElf(input: List<String>): Map<Int, Int> {
+        var currentElfIndex = 0
+        val elfCalorieCounts = mutableMapOf<Int, Int>()
+        input.forEach {
+            if (it.isEmpty()) {
+                currentElfIndex++
+                return@forEach
+            }
+
+            val currentCalorieCount = elfCalorieCounts.getOrDefault(currentElfIndex, 0)
+            elfCalorieCounts[currentElfIndex] = currentCalorieCount + it.toInt()
+        }
+        return elfCalorieCounts
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return calorieCountsByElf(input).values.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return calorieCountsByElf(input).values.sorted().takeLast(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
